e4163723bc9a44e70f88489c45d62dbb7c8982a5
sknopp7539 <STACYKNOPP1@GMAIL.COM>
2025-10-31 22:06:13 -0400

    Refactor App component to include routing and providers

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,45 @@ import { Route, Switch } from "wouter";
 import ErrorBoundary from "@/components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import { ProviderProvider } from "./contexts/ProviderContext";
+import { Toaster } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import NotFound from "@/pages/NotFound";
+import { Route, Switch } from "wouter";
+import ErrorBoundary from "@/components/ErrorBoundary";
+import { ThemeProvider } from "./contexts/ThemeContext";
+import { ProviderProvider } from "./contexts/ProviderContext";
+
+import Home from "./pages/Home";
+import Reports from "./pages/Reports";
+import Settings from "./pages/Settings";
+import UserManagement from "./pages/UserManagement";
+
+function Router() {
+  return (
+    <Switch>
+      <Route path="/" component={Home} />
+      <Route path="/reports" component={Reports} />
+      <Route path="/settings" component={Settings} />
+      <Route path="/users" component={UserManagement} />
+      <Route path="/:rest*" component={NotFound} />
+    </Switch>
+  );
+}
+
+export default function App() {
+  return (
+    <ErrorBoundary>
+      <ThemeProvider defaultTheme="light">
+        <ProviderProvider>
+          <TooltipProvider>
+            <Toaster />
+            <Router />
+          </TooltipProvider>
+        </ProviderProvider>
+      </ThemeProvider>
+    </ErrorBoundary>
+  );
+}
 
 // PAGES that currently exist
 import Home from "./pages/Home";
